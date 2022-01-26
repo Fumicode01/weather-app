1a80e41124f3899a330ea330e346ef08391ab704
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2022-01-26 23:55:30 +1100

    Init tailwind css

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+      <>
+          <div className="bg-black text-3xl text-white">App</div>
+      </>
+
+  )
 };
 
 export default App;
