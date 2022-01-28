cd47032d8d8e65104d62fbfca149dd70d5696da1
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2022-01-28 16:28:05 +1100

    Fixed style

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -13,11 +13,11 @@ const Card = () => {
   return (
         <div className="flex">
             {forecastWeathers?.map((weather) => (
-                <div key={weather.date} className="w-64 cursor-pointer border b-gray-400 rounded-3xl flex flex-col justify-center items-center text-center p-6 bg-slate-100">
+                <div key={weather.date} className="w-48 cursor-pointer border b-gray-400 rounded-3xl flex flex-col justify-center items-center text-center p-6 bg-slate-100">
                     <div className="text-md font-bold flex flex-col text-gray-900">
                         <span className="uppercase">{getDayOfWeek(weather.date)}</span> 
                     </div>
-                    <div className="w-32 h-32 flex items-center justify-center">
+                    <div className="w-24 h-16 flex items-center justify-center">
                         <img src={weather.icon} alt="" />
                     </div>
                     <p className="text-gray-700 mb-2">{weather.condition}</p>
